d45c5fa50d64a85a1950d93b1a54a6fec319d17c
LucasDSanti <lucasdesanti2002@gmail.com>
2024-07-06 08:47:47 -0300

    validacao-estadioUicc

--- automacao/automacao.py
+++ automacao/automacao.py
@@ -1,12 +1,59 @@
 import pyautogui
 
-contador: int
-contador = 0
+contadorFormulario: int
+contadorFormulario = 1
 
-while contador <= 20:
+contadorEnter: int
 
-    pyautogui.PAUSE = 0.5
+contadorMedicamento: int
 
-    pyautogui.press('enter')
+pyautogui.alert('IREI COMEÇAR A DAR ENTER EM 3 SEGUNDOS!')
+pyautogui.PAUSE = 3
 
-    contador += 1
+while contadorFormulario <= 5:
+
+    contadorEnter = 1
+    contadorMedicamento = 1
+
+    while contadorEnter <= 34:
+
+        pyautogui.press('enter')
+        pyautogui.PAUSE = 0.5
+
+        contadorEnter += 1
+
+    pyautogui.press('esc')
+
+    contadorEnter = 1
+
+    while contadorEnter <= 24:
+
+        pyautogui.press('enter')
+
+        if contadorEnter == 3:
+
+            try: pyautogui.locateOnScreen('estadioUicc.png')
+        
+            except: print("NÃO ACHEI!")
+
+            else: 
+                print("ACHEI!") 
+                contadorEnter += 1
+
+        if contadorEnter == 13 and contadorMedicamento == 1:
+
+            try: pyautogui.locateAllOnScreen('medicamento4.png')
+
+            except: 
+                print("NÃO ACHEI!")
+                contadorEnter = contadorEnter - 4
+                contadorMedicamento += 1
+
+            else: 
+                print("ACHEI!")
+
+        contadorEnter += 1
+
+    pyautogui.press('down')
+
+    contadorFormulario += 1
